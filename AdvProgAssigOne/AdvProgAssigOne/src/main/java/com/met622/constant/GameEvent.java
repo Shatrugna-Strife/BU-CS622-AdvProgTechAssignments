7c67aa7ee71f86a8a0f1f175032bffd038eaf1cd
Shatrugna-Strife <shatrugnakorukanti@gmail.com>
2022-09-13 12:49:31 -0400

    Revert "Assigment1 - Done I guess"
    This reverts commit a29a43f3c6497446d491c422507af07fc53d7c05.

--- AdvProgAssigOne/AdvProgAssigOne/src/main/java/com/met622/constant/GameEvent.java
+++
@@ -1,8 +0,0 @@
-package com.met622.constant;
-
-/**
- * Possible game states or events
- */
-public enum GameEvent {
-    PLAYER_ONE_TURN, PLAYER_TWO_TURN, BANANA_THROW, BANANA_HIT_WALL, BANANA_HIT_PLAYER,
-}
--- AdvProgAssigOne/AdvProgAssigOne/src/main/java/com/met622/model/GameEventAction.java
+++
@@ -1,9 +0,0 @@
-package com.met622.model;
-
-import com.met622.entity.GameInstance;
-import com.met622.entity.SwingGui;
-import processing.core.PApplet;
-
-public interface GameEventAction {
-    public void executeAction(PApplet game, SwingGui swingGui, GameInstance gameInstance);
-}
