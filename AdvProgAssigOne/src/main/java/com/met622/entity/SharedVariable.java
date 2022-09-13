94642490d98166cf615f6b463886b3240630e60c
Shatrugna-Strife <shatrugnakorukanti@gmail.com>
2022-09-13 13:40:10 -0400

    Assignment1- Done

--- AdvProgAssigOne/src/main/java/com/met622/entity/SharedVariable.java
+++
@@ -1,7 +0,0 @@
-package com.met622.entity;
-
-public class SharedVariable {
-    public static Integer angle = null;
-    public static Integer velocity = null;
-    public static Boolean enterClicked = null;
-}
